3a12a815561ffeae562de44029214728924f6e58
casablanca <evaseemefly@126.com>
2017-12-09 22:32:19 +0800

    添加部分内容

--- chapter4/adapter.py
+++ chapter4/adapter.py
@@ -27,9 +27,11 @@ def main():
     objects = [Computer('Asus')]
     synth = Synthesizer('moog')
     objects.append(Adapter(synth, dict(execute=synth.play)))
-    human = Human('Bob')
-    objects.append(Adapter(human, dict(execute=human.speak)))
+    test=Adapter(synth, dict(execute=synth.play))
 
+    print(test.execute())
+    human = Human('Bob')
+    objects.append(Adapter(human,dict(excute=human.speak)))
     for i in objects:
         print('{} {}'.format(str(i), i.execute()))
 
