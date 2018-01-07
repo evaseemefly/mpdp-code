e4a1ec636f0ccc5c49b37104bc8f8dbb10524266
casablanca <evaseemefly@126.com>
2018-01-08 06:44:57 +0800

    加入部分备注

--- chapter10/chain.py
+++ chapter10/chain.py
@@ -1,5 +1,7 @@
 class Event:
-
+    '''
+    描述一个事件
+    '''
     def __init__(self, name):
         self.name = name
 
@@ -8,14 +10,31 @@ class Event:
 
 
 class Widget:
+    '''
+    所有操作的基类，所有操作都要继承自该类
+    '''
 
     def __init__(self, parent=None):
+        '''
+        默认的构造函数parent可以为None
+        :param parent:
+        '''
         self.parent = parent
 
     def handle(self, event):
+        '''
+        所有的子类，都继承自父类，都可能调用父类的handle方法
+        handle方法会处理传入的事件
+        :param event:
+        :return:
+        '''
         handler = 'handle_{}'.format(event)
+        # 判断传入的事件，当前对象是否存在指定方法
         if hasattr(self, handler):
+            # 取到当前类的指定方法
             method = getattr(self, handler)
+            # 执行该方法
+            # 对传入的event事件进行处理
             method(event)
         elif self.parent:
             self.parent.handle(event)
@@ -49,6 +68,9 @@ def main():
     sd = SendDialog(mw)
     msg = MsgText(sd)
 
+    '''
+        由于实例化是通过名字来创建具体的事件
+    '''
     for e in ('down', 'paint', 'unhandled', 'close'):
         evt = Event(e)
         print('\nSending event -{}- to MainWindow'.format(evt))
